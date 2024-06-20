4a6bf6e0bfda9ae63b1c0b6d954ff99a9144f33c
JUAN <mateo@cardenas.com>
2024-06-20 09:59:05 -0500

    SI

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,6 +7,20 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+
+        String primerNumero= JOptionPane.showInputDialog("INTRODUCE UN NUMERO");
+        String segundoNumero= JOptionPane.showInputDialog("INTRODUCE UN segundo NUMERO");
+
+        int numero1= Integer.parseInt(primerNumero);
+        int numero2= Integer.parseInt(segundoNumero);
+        int resultado= numero1*numero2;
+
+
+
+        JOptionPane.showMessageDialog(null, "El resultado de la aplicacion es: "+resultado);
+
+
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
